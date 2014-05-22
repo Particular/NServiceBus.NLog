9abaf47968f12c3ae410ad2259f61c0982d9de01
Simon Cropp <simon.cropp@gmail.com>
2014-05-22 21:22:28 +1000

    init

---
+++ NServiceBus.NLog/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NServiceBus Logging NLog ")]
+[assembly: AssemblyDescription("Implementation of logging functionality on top of NLog.")]
+[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
+[assembly: AssemblyProduct("NServiceBus")]
+[assembly: AssemblyCompany("NServiceBus Ltd.")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
