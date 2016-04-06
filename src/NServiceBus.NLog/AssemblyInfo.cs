00d5ccdab5d1532b1923446a7c5edf0a1d64def8
Simon Cropp <simon.cropp@gmail.com>
2016-04-06 17:29:43 +1000

    fix copyright

--- src/NServiceBus.NLog/AssemblyInfo.cs
+++ src/NServiceBus.NLog/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("NServiceBus NLog integration")]
 [assembly: AssemblyDescription("Implementation of logging functionality on top of NLog.")]
-[assembly: AssemblyCopyright("Copyright 2010-2016 NServiceBus. All rights reserved")]
+[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
 [assembly: AssemblyProduct("NServiceBus")]
 [assembly: AssemblyCompany("NServiceBus Ltd.")]
 [assembly: CLSCompliant(true)]
